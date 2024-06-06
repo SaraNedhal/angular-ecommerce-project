6b38754dd6c929fd6b13f94aee8bb68e12ae107d
saranedhal <sarahnedhal@gmail.com>
2024-06-06 17:19:36 +0300

    intial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,RouterModule,  HeaderComponent, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular-ecommerce-project';
+}
